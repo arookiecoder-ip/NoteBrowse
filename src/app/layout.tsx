99d64747ef5ad1131068466315dc2f59287f6db3
Jit Karmakar <jit20karmakar@gmail.com>
2026-04-17 15:50:06 +0530

    feat: initialize root layout with global styles, fonts, and header/footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
               arookiecoder-ip
             </a>
           </div>
-          <span>Last Patched: Fri, 17 Apr 2026 14:06 IST</span>
+          <span>Last Patched: Fri, 17 Apr 2026 15:49 IST</span>
         </footer>
       </body>
     </html>
